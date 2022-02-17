e796ed09145183b6ede3eae0c91e9caf5c57b4a4
Djosc <davidjoscaldwell@gmail.com>
2022-02-17 16:46:36 -0500

    Setup Emailjs for contact form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import emailjs from '@emailjs/browser';
+import { init } from '@emailjs/browser';
 
 import {
 	FooterContainer,
@@ -23,6 +26,53 @@ import {
 import { Button } from '../Button/Button';
 
 const Footer = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [subject, setSubject] = useState('');
+	const [message, setMessage] = useState('');
+	const [emailSent, setEmailSent] = useState(false);
+
+	// init(process.env.REACT_APP_User_ID);
+
+	const submit = () => {
+		if (name && email && message) {
+			const serviceId = 'service_58slowi';
+			const templateId = 'template_vg7h2in';
+			const templateParams = {
+				name,
+				email,
+				subject,
+				message,
+			};
+
+			if (isValidEmail(email)) {
+				emailjs
+					.send(serviceId, templateId, templateParams, process.env.REACT_APP_User_ID)
+					.then((response) => {
+						console.log(response);
+						alert(
+							'Thank you for your message. I will get back with you as soon as possible.'
+						);
+					})
+					.catch((error) => console.log(error));
+			}
+
+			setName('');
+			setEmail('');
+			setSubject('');
+			setMessage('');
+			setEmailSent(true);
+		} else {
+			alert('Please fill in all fields');
+		}
+	};
+
+	const isValidEmail = (email) => {
+		const regex =
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return regex.test(String(email).toLowerCase());
+	};
+
 	const openGitHub = () => {
 		const url = 'https://github.com/Djosc';
 		window.open(url, '_blank');
@@ -58,12 +108,36 @@ const Footer = () => {
 					<FooterContactWrap>
 						<FooterContact>
 							<FormH1>Send me an email</FormH1>
-							<FormInput type="text" placeholder="Name" name="name" />
-							<FormInput type="email" placeholder="Email" name="email" />
-							<FormInput type="text" placeholder="Subject" name="subject" />
-							<FormInputArea type="text" placeholder="Message" name="message" />
+							<FormInput
+								type="text"
+								placeholder="Your Name"
+								name="name"
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+							<FormInput
+								type="email"
+								placeholder="Your Email"
+								name="email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							<FormInput
+								type="text"
+								placeholder="Subject"
+								name="subject"
+								value={subject}
+								onChange={(e) => setSubject(e.target.value)}
+							/>
+							<FormInputArea
+								type="text"
+								placeholder="Message"
+								name="message"
+								value={message}
+								onChange={(e) => setMessage(e.target.value)}
+							/>
 							<BtnWrap>
-								<Button primary="true" dark="true" big="true">
+								<Button primary="true" dark="true" big="true" onClick={submit}>
 									Send
 								</Button>
 							</BtnWrap>
